96685f4e0946fda94a1dd63e151aa317af964fc6
akibria12 <ahkibria@hotmail.co.uk>
2023-06-10 12:12:28 +0100

    my first day of python learning

---
+++ app.py
@@ -0,0 +1,48 @@
+age = 20
+age = 30
+price = 19.95
+first_name = "Abul"
+last_name = " Kibria"
+
+is_online = False
+
+# name = input("What is your name? ")
+# birth_year = input("Enter your birth year: ")
+# int(birth_year)
+# age = 2022 - int(birth_year)
+# print(age)
+
+
+# print("Hello " + name)
+
+int()
+float()
+bool()
+
+# <adding numbers>
+# first_number = input("First: ")
+# second_number = input("Second: ")
+# float(first_number)
+# float(second_number)
+# sum = float(first_number) + float(second_number)
+# print("Sum: " + str(sum))
+
+# Strings
+course = " Python for Beginners"
+print(course.upper())
+print(course)
+print(course.find("for"))
+print(course.replace("for", "4"))
+print(course.lower())
+print(course.find("Beginners"))
+print("Python" in course)
+
+print(10 + 3)
+print(10 // 3)
+x = 10 + 3 * 2
+print("The value of x is : " + str(x))
+
+# x = 3 < 2
+# print(x)
+price = 5
+print(price > 10 or price < 30)
